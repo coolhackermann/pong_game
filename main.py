4bdb3e14ef6b2be00570f11d14fa483f7000779b
coolhackermann <73700746+coolhackermann@users.noreply.github.com>
2021-11-14 17:15:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-with open('main.txt', 'r') as f:
+with open('assets/main.txt', 'r') as f:
     x = f.read()
     #print(x)
 
 
 
-    with open('main.txt', 'w') as f2:
+    with open('assets/main.txt', 'w') as f2:
             if x == '0':
                 f2.write('1')
-                import pong.py
+                from assets import pong.py
             elif x == '1':
                 f2.write('2')
-                import pong.py
+                from assets import pong.py
             elif x == '2':
                 f2.write('3')
-                import pong.py
+                from assets import pong.py
             elif x == '3':
                 f2.write('4')
                 import pong.py
             elif x == '4':
                 f2.write('0')
-                import emoji.py
+                from assets import emoji.py
 
-with open('main.txt', 'r') as check:
+with open('assets/main.txt', 'r') as check:
     x = check.read()
     print(x)
 
